5789cc000fe69824b1660eb9f12ba1b6cf58895b
zarshab <zarshabrafiq1092@gmail.com>
2023-05-03 22:32:45 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
             <Route index element={<Home/>}/>
             <Route path='login' element={<Login/>}/>
             <Route path='register' element={<Register/>}/>
+            <Route path='*' element={<h1>Page not found</h1>}/>
        </Route>
 
        <Route path='/dashboard'element={<Dashboard/>}>
